20c0e4bb92d595a9bea7792ed4b5eb396f49eff4
Luis Rodriguez <105326568+Lrodriguez310@users.noreply.github.com>
2022-12-11 01:44:07 -0500

    added gallery.html
    images appear when you scroll mouse. Javascript

--- Mallard-Website-main (3)/Mallard-Website-main/MallardWeb/main.js
+++ Mallard-Website-main (3)/Mallard-Website-main/MallardWeb/main.js
@@ -47,3 +47,57 @@ const closeNav = () => {
 }
 
 closeBtn.addEventListener('click', closeNav)
+
+
+
+
+
+const images = document.getElementsByClassName("image");
+
+let globalIndex = 0;
+    last = { x:0, y:0 };
+
+const activate = (image, x, y) => {
+    image.style.left = `${x}px`;
+    image.style.top = `${y}px`;
+
+    image.dataset.status = "active";
+
+    last = { x, y };
+}
+
+const distanceFromLast = (x, y) => {
+    return Math.hypot(x - last.x, y - last.y);
+}
+
+window.onmousemove = e => {
+
+    if(distanceFromLast(e.clientX, e.clientY) > 100) {
+    const lead = images[globalIndex % images.length];
+          tail = images[(globalIndex - 5) % images.length];
+
+    activate(lead, e.clientX, e.clientY);
+
+    if(tail) tail.dataset.status = "inactive";
+
+    globalIndex++;
+  }
+}
+
+// for bubble
+
+/*const wrapper = document.getElementById("bubble-wrapper");
+
+const animateBubble = x => {
+    const bubble = document.createElement("div");
+
+    bubble.className = "bubble";
+
+    bubble.style.left = `${x}px`;
+
+    wrapper.appendChild(bubble);
+
+    setTimeout(() => wrapper.removeChild(bubble), 2000);
+}
+
+window.onmousemove = e => animateBubble(e.clientX); */
